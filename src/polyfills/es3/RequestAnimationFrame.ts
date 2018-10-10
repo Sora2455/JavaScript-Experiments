bd8976cc0ba660ef06c1cfeb25dc436319c1a139
Sora2455 <boreenpt@gmail.com>
2018-10-10 20:53:56 +1000

    Reduce errors

--- src/polyfills/es3/RequestAnimationFrame.ts
+++ src/polyfills/es3/RequestAnimationFrame.ts
@@ -7,17 +7,17 @@
 
 (function() {
     let lastTime = 0;
-    
+
     if (!window.requestAnimationFrame)
         window.requestAnimationFrame = function(callback: FrameRequestCallback): number {
             const currTime = new Date().getTime();
             const timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            const id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+            const id = window.setTimeout(function() { callback(currTime + timeToCall); },
               timeToCall);
             lastTime = currTime + timeToCall;
             return id;
         };
- 
+
     if (!window.cancelAnimationFrame)
         window.cancelAnimationFrame = function(id: number): void {
             clearTimeout(id);
--- src/polyfills/es5/BroadcastChannel.ts
+++ src/polyfills/es5/BroadcastChannel.ts
@@ -9,29 +9,29 @@ interface Window {
  * @see https://gist.github.com/inexorabletash/52f437d1451d12145264
  */
 class BCPollyfill {
-    static channels = {} as channelList;
+    private static channels = {} as channelList;
     private _name: string;
     private _id: string;
     private _closed: boolean;
     private _mc: MessageChannel;
-  
+
     constructor(channel: string) {
         const $this = this;
         channel = String(channel);
-    
-        const id = '$BroadcastChannel$' + channel + '$';
-    
+
+        const id = "$BroadcastChannel$" + channel + "$";
+
         BCPollyfill.channels[id] = BCPollyfill.channels[id] || [];
         BCPollyfill.channels[id].push(this);
-    
+
         this._name = channel;
         this._id = id;
         this._closed = false;
         this._mc = new MessageChannel();
         this._mc.port1.start();
         this._mc.port2.start();
-    
-        self.addEventListener('storage', function(e) {
+
+        self.addEventListener("storage", function(e) {
             if (e.storageArea !== self.localStorage) return;
             if (e.newValue === null) return;
             if (e.key.substring(0, id.length) !== id) return;
@@ -41,17 +41,17 @@ class BCPollyfill {
     }
 
     get name() { return this._name; }
-    postMessage(message: any): void {
+    public postMessage(message: any): void {
         const $this = this;
         if (this._closed) {
             const e = new Error();
-            e.name = 'InvalidStateError';
+            e.name = "InvalidStateError";
             throw e;
         }
         const value = JSON.stringify(message);
 
         // Broadcast to other contexts via storage events...
-        const key = this._id + String(Date.now()) + '$' + String(Math.random());
+        const key = this._id + String(Date.now()) + "$" + String(Math.random());
         self.localStorage.setItem(key, value);
         setTimeout(function() { self.localStorage.removeItem(key); }, 500);
 
@@ -61,7 +61,7 @@ class BCPollyfill {
             bc._mc.port2.postMessage(JSON.parse(value));
         });
     }
-    close(): void {
+    public close(): void {
         if (this._closed) return;
         this._closed = true;
         this._mc.port1.close();
@@ -74,13 +74,15 @@ class BCPollyfill {
     // EventTarget API
     get onmessage() { return this._mc.port1.onmessage; }
     set onmessage(value) { this._mc.port1.onmessage = value; }
-    addEventListener(type: "message" | "messageerror", listener: (this: MessagePort, ev: MessageEvent) => any /*, useCapture*/): void {
+    public addEventListener(type: "message" | "messageerror",
+                            listener: (this: MessagePort, ev: MessageEvent) => any /*, useCapture*/): void {
         return this._mc.port1.addEventListener.apply(this._mc.port1, arguments);
     }
-    removeEventListener(type: "message" | "messageerror", listener: (this: MessagePort, ev: MessageEvent) => any /*, useCapture*/): void {
+    public removeEventListener(type: "message" | "messageerror",
+                               listener: (this: MessagePort, ev: MessageEvent) => any /*, useCapture*/): void {
         return this._mc.port1.removeEventListener.apply(this._mc.port1, arguments);
     }
-    dispatchEvent(event: Event): void {
+    public dispatchEvent(event: Event): void {
         return this._mc.port1.dispatchEvent.apply(this._mc.port1, arguments);
     }
 }
