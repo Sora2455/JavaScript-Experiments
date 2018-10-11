d9e2cf2781f17e15919e3d55225730993fdd9ef8
Sora2455 <boreenpt@gmail.com>
2018-10-11 22:14:41 +1000

    Fix print CSS issues with JavaScript

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,28 @@
 import "./modules/LazyLoad.js";
 import "./modules/Localiser.js";
 import {onBuyClicked} from "./modules/PaymentRequest.js";
+import {PrintManager} from "./modules/PrintManager.js";
 import {ReadyManager} from "./modules/readyManager.js";
 // import {loadBytecode} from "./modules/LoadBytecode.js";
 
-const manager = new ReadyManager();
+const readyManager = new ReadyManager();
 /*manager.whenReady(() => {
     loadBytecode("/build/bytecode/demo.wasm", window, null, 1, exports => {
         console.log(exports._my_add(1, 2));
     });
 });*/
-manager.whenReady(() => {
+readyManager.whenReady(() => {
     const buyTest = document.getElementById("buyTest");
     if (buyTest) {
         buyTest.onclick = onBuyClicked;
     }
+});
+const printManager = new PrintManager();
+printManager.RunBeforePrint(() => {
+    const visibleId = location.hash.substr(1) || "1";
+    document.getElementById(visibleId).setAttribute("data-print-subject", "");
+});
+printManager.RunAfterPrint(() => {
+    const visibleId = location.hash.substr(1) || "1";
+    document.getElementById(visibleId).removeAttribute("data-print-subject");
 });
---
+++ src/modules/PrintManager.ts
@@ -0,0 +1,64 @@
+export class PrintManager {
+    private runTheseBeforePrint = [] as Array<() => void>;
+    private runTheseAfterPrint = [] as Array<() => void>;
+
+    constructor() {
+        this.OnMediaChange = this.OnMediaChange.bind(this);
+        this.OnPrintBegin = this.OnPrintBegin.bind(this);
+        this.OnPrintEnd = this.OnPrintEnd.bind(this);
+
+        if (typeof onbeforeprint !== "undefined") {
+            // Before/After print is what we're actually after
+            addEventListener("beforeprint", this.OnPrintBegin);
+            addEventListener("afterprint", this.OnPrintEnd);
+        } else {
+            // ...but Safari doesn't support them, so we use matchMedia instead
+            matchMedia("print").addListener(this.OnMediaChange);
+        }
+    }
+
+    /**
+     * Queue an action to run before the page is printed
+     * @param action The action to run before the page is printed
+     */
+    public RunBeforePrint(action: () => void): void {
+        this.runTheseBeforePrint.push(action);
+    }
+
+    /**
+     * Queue an action to run after the page is printed or the print attempt is canceled
+     * @param action The action to run before the page is printed
+     */
+    public RunAfterPrint(action: () => void): void {
+        this.runTheseAfterPrint.push(action);
+    }
+
+    /**
+     * Handles the case where the "printing" status changes
+     */
+    private OnMediaChange(ev: any) {
+        if (ev.matches) {
+            this.OnPrintBegin();
+        } else {
+            this.OnPrintEnd();
+        }
+    }
+
+    /**
+     * Handle the begining of a print attempt
+     */
+    private OnPrintBegin() {
+        for (let i = this.runTheseBeforePrint.length; i--;) {
+            this.runTheseBeforePrint[i]();
+        }
+    }
+
+    /**
+     * Handle the completion or calcelation of a print attempt
+     */
+    private OnPrintEnd() {
+        for (let i = this.runTheseAfterPrint.length; i--;) {
+            this.runTheseAfterPrint[i]();
+        }
+    }
+}
