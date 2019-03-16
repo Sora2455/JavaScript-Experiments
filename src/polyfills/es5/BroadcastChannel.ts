55f0b5599d3497835da1c3508efe544abf479159
Sora2455 <boreenpt@gmail.com>
2019-03-17 08:00:38 +1000

    Reduce TSLint errors

--- src/polyfills/es5/BroadcastChannel.ts
+++ src/polyfills/es5/BroadcastChannel.ts
@@ -1,5 +1,5 @@
 "use strict";
-interface channelList {
+interface IChannelList {
     [index: string]: BCPollyfill[];
 }
 interface Window {
@@ -9,14 +9,13 @@ interface Window {
  * @see https://gist.github.com/inexorabletash/52f437d1451d12145264
  */
 class BCPollyfill {
-    private static channels = {} as channelList;
+    private static channels = {} as IChannelList;
     private _name: string;
     private _id: string;
     private _closed: boolean;
     private _mc: MessageChannel;
 
     constructor(channel: string) {
-        const $this = this;
         channel = String(channel);
 
         const id = "$BroadcastChannel$" + channel + "$";
@@ -31,18 +30,17 @@ class BCPollyfill {
         this._mc.port1.start();
         this._mc.port2.start();
 
-        self.addEventListener("storage", function(e) {
-            if (e.storageArea !== self.localStorage) return;
-            if (e.newValue === null) return;
-            if (e.key.substring(0, id.length) !== id) return;
+        self.addEventListener("storage", (e) => {
+            if (e.storageArea !== self.localStorage) { return; }
+            if (e.newValue === null) { return; }
+            if (e.key.substring(0, id.length) !== id) { return; }
             const data = JSON.parse(e.newValue);
-            $this._mc.port2.postMessage(data);
+            this._mc.port2.postMessage(data);
         });
     }
 
     get name() { return this._name; }
     public postMessage(message: any): void {
-        const $this = this;
         if (this._closed) {
             const e = new Error();
             e.name = "InvalidStateError";
@@ -53,16 +51,16 @@ class BCPollyfill {
         // Broadcast to other contexts via storage events...
         const key = this._id + String(Date.now()) + "$" + String(Math.random());
         self.localStorage.setItem(key, value);
-        setTimeout(function() { self.localStorage.removeItem(key); }, 500);
+        setTimeout(() => { self.localStorage.removeItem(key); }, 500);
 
         // Broadcast to current context via ports
-        BCPollyfill.channels[this._id].forEach(function(bc) {
-            if (bc === $this) return;
+        BCPollyfill.channels[this._id].forEach((bc) => {
+            if (bc === this) { return; }
             bc._mc.port2.postMessage(JSON.parse(value));
         });
     }
     public close(): void {
-        if (this._closed) return;
+        if (this._closed) { return; }
         this._closed = true;
         this._mc.port1.close();
         this._mc.port2.close();
