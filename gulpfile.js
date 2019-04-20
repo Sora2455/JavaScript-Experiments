92569611ec6edf95a012a6063d6f7b7aec4c857f
Sora2455 <boreenpt@gmail.com>
2019-04-20 16:26:49 +1000

    Replace gifs with silent videos

--- gulpfile.js
+++ gulpfile.js
@@ -21,12 +21,18 @@ const path = require('path');
 const {MathMlReplacer} = require("math-ml-now");
 const imagemin = require('gulp-imagemin');
 const imageminWebp = require('imagemin-webp');
+const ffmpeg = require('fluent-ffmpeg');
 const exec = require('child_process').exec;
 const realFavicon = require('gulp-real-favicon');
 const fs = require('fs');
 
 // File where the favicon markups are stored
 const FAVICON_DATA_FILE = 'src/favicon/faviconData.json';
+// Folder where the ffmeg binaries are stored
+const configJson = JSON.parse(fs.readFileSync("config.json"));
+ffmpeg.setFfmpegPath(configJson.ffmpegPath);
+ffmpeg.setFfprobePath(configJson.ffprobePath);
+const gifConvertOptions = "[0:v] fps=12,scale=320:-1:flags=lanczos,split [a][b];[a] palettegen [p];[b][p] paletteuse";
 
 const polyfillRegex = /\/\/ startpolyfill[\s\S]*?\/\/ endpolyfill/g;
 
@@ -177,11 +183,11 @@ gulp.task("polyfillsEs5", function() {
 
 gulp.task("polyfills", gulp.parallel("polyfillsEs3", "polyfillsEs5"));
 
-gulp.task("compileBytecode", function(cb) {
+gulp.task("compileBytecode", function(done) {
     exec('cd src && make.bat', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
-        cb(err);
+        done(err);
     });
 });
 
@@ -269,6 +275,39 @@ gulp.task("mediaImages", function(){
         .pipe(gulp.dest("build/media"));
 });
 
+gulp.task("silentVideos", function(done){
+    const files = glob.sync("src/media/silentVideos/*{webm,ogv}");
+    const promises = files.map(filePath => {
+        const fileName = path.basename(filePath, path.extname(filePath));
+        //If we've already generated this video, skip
+        if (fs.existsSync(`build/media/${fileName}.webm`) &&
+            //fs.existsSync(`build/media/${fileName}.ogv`) &&
+            fs.existsSync(`build/media/${fileName}.gif`)) {
+            return;
+        }
+        return new Promise((resolve, reject) => {
+            ffmpeg(filePath)
+                .output(`build/media/${fileName}.webm`)
+                .noAudio()
+                .format("webm")
+                .videoCodec("libvpx")
+                //.output(`build/media/${fileName}.ogv`)
+                //.noAudio()
+                //.format("ogv")
+                //.videoCodec("libtheora")
+                .output(`build/media/${fileName}.gif`)
+                .outputOptions("-vf", gifConvertOptions)
+                .on("error", function(err, stdout, stderr) {
+                    console.log("Cannot process video: " + err.message);
+                    reject();
+                })
+                .on("end", resolve)
+                .run();
+        });
+    });
+    Promise.all(promises).then(() => { done(); }).catch(done);
+});
+
 gulp.task("media", gulp.parallel("mediaWebP", "mediaImages"));
 
 // Generate the icons. This task takes a few seconds to complete.
@@ -348,7 +387,7 @@ gulp.task('generate-favicon', function(done) {
 // released a new Touch icon along with the latest version of iOS).
 // Run this task from time to time. Ideally, make it part of your
 // continuous integration system.
-gulp.task('check-for-favicon-update', function(done) {
+gulp.task('check-for-favicon-update', function() {
 	var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
 	realFavicon.checkForUpdates(currentVersion, function(err) {
 		if (err) {
