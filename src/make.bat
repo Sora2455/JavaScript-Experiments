24659eb7fb18f2991444b8759de3cf41823e4a3a
Sora2455 <boreenpt@gmail.com>
2020-02-09 17:49:02 +1000

    Remove options that have no effect

--- src/make.bat
+++ src/make.bat
@@ -5,4 +5,4 @@ cd "c code"
 rem Build asm.js for old browsers
 for %%i in (*) do start cmd.exe @cmd /k "emcc %%~nxi -O3 -s INVOKE_RUN=0 -s ENVIRONMENT='worker' -s WASM=0 -s MINIMAL_RUNTIME=1 -o ../../build/bytecode/%%~ni.asm.js && exit || echo error in %%~nxi"
 rem Build WebAssembly for new browsers
-for %%i in (*) do start cmd.exe @cmd /k "emcc %%~nxi -O3 -s INVOKE_RUN=0 -s ENVIRONMENT='worker' -s WASM=1 -s MINIMAL_RUNTIME=1 -o ../../build/bytecode/%%~ni.wasm && exit || echo error in %%~nxi"
+for %%i in (*) do start cmd.exe @cmd /k "emcc %%~nxi -O3 -s WASM=1 -o ../../build/bytecode/%%~ni.wasm && exit || echo error in %%~nxi"
