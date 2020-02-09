3fd2daf2200b0c2b1dfe360316f0a35d5e1b826f
Sora2455 <boreenpt@gmail.com>
2020-02-09 15:07:48 +1000

    Remove NO_DYNAMIC_EXECUTION, as that option was removed

--- src/make.bat
+++ src/make.bat
@@ -3,6 +3,6 @@ if not exist "..\build\bytecode\" mkdir "..\build\bytecode\"
 cd "c code"
 
 rem Build asm.js for old browsers
-for %%i in (*) do start cmd.exe @cmd /k "emcc %%~nxi -O3 -s INVOKE_RUN=0 -s ENVIRONMENT='worker' -s WASM=0 -s NO_DYNAMIC_EXECUTION=1 -s MINIMAL_RUNTIME=1 -s MIN_IE_VERSION=10 -o ../../build/bytecode/%%~ni.asm.js && exit || echo error in %%~nxi"
+for %%i in (*) do start cmd.exe @cmd /k "emcc %%~nxi -O3 -s INVOKE_RUN=0 -s ENVIRONMENT='worker' -s WASM=0 -s MINIMAL_RUNTIME=1 -s MIN_IE_VERSION=10 -o ../../build/bytecode/%%~ni.asm.js && exit || echo error in %%~nxi"
 rem Build WebAssembly for new browsers
 for %%i in (*) do start cmd.exe @cmd /k "emcc %%~nxi -O0 -s INVOKE_RUN=0 -s ENVIRONMENT='worker' -s WASM=1 -s MINIMAL_RUNTIME=1 -o ../../build/bytecode/%%~ni.wasm.js && exit || echo error in %%~nxi"
