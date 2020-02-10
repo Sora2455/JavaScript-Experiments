cc8c6c89f2bf7c3d4a3bbf80fbbef7d2dfcf2c2c
Sora2455 <boreenpt@gmail.com>
2020-02-11 05:55:53 +1000

    Enable legacy JavaScript engine support for Asm.Js (new browsers will use Wasm anyway)

--- src/make.bat
+++ src/make.bat
@@ -3,6 +3,6 @@ if not exist "..\build\bytecode\" mkdir "..\build\bytecode\"
 cd "c code"
 
 rem Build asm.js for old browsers
-for %%i in (*) do start cmd.exe @cmd /k "emcc %%~nxi -O3 -s INVOKE_RUN=0 -s ENVIRONMENT='worker' -s WASM=0 -s MINIMAL_RUNTIME=1 -o ../../build/bytecode/%%~ni.asm.js && exit || echo error in %%~nxi"
+for %%i in (*) do start cmd.exe @cmd /k "emcc %%~nxi -O3 -s WASM=0 -s INVOKE_RUN=0 -s ENVIRONMENT='worker' -s MINIMAL_RUNTIME=1 -s LEGACY_VM_SUPPORT=1 -o ../../build/bytecode/%%~ni.asm.js && exit || echo error in %%~nxi"
 rem Build WebAssembly for new browsers
 for %%i in (*) do start cmd.exe @cmd /k "emcc %%~nxi -O3 -s WASM=1 -o ../../build/bytecode/%%~ni.wasm && exit || echo error in %%~nxi"
