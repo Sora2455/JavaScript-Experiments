64335785c606a90b928244dea58b18416d028239
Sora2455 <boreenpt@gmail.com>
2020-02-22 07:10:51 +1000

    Remove lazy youtube test (I was required to whitelist too much)

--- gulpfile.js
+++ gulpfile.js
@@ -319,34 +319,6 @@ function addShareWidget() {
             </page-share>`;
 }
 
-/**
- * Adds a simple, no JS YouTube widget that only loads an image until clicked on
- */
-function addLazyYouTube(element) {
-    const codeRegex = /code="(.+?)"/;
-    // TODO what if the title has a double or single quote?
-    const titleRegex = /title="(.+?)"/;
-    const videoCode = element.match(codeRegex)[1];
-    const videoTitle = element.match(titleRegex)[1];
-    return `<iframe ` +
-                `width="560" ` +
-                `height="315" ` +
-                `loading="lazy" ` +
-                `lazyload="1" ` +
-                `referrerpolicy="no-referrer" ` +
-                `src="https://www.youtube.com/embed/${videoCode}" ` +
-                `srcdoc="<link rel='stylesheet' href='lazyYouTube.css' /> ` +
-                `<a href=https://www.youtube.com/embed/${videoCode}?autoplay=1> ` +
-                    `<img src=https://img.youtube.com/vi/${videoCode}/hqdefault.jpg ` +
-                        `alt='Video ${videoTitle}' width='480' height='360'> ` +
-                    `<span>▶</span> ` +
-                `</a>" ` +
-                `frameborder="0" ` +
-                `allow="autoplay; encrypted-media; fullscreen; picture-in-picture" ` +
-                `allowfullscreen ` +
-                `title="${videoTitle}"></iframe>`;
-}
-
 gulp.task("html", function() {
     return gulp.src("src/html/*.html")
                 .pipe(replace("/src/", "/"))
@@ -354,7 +326,6 @@ gulp.task("html", function() {
                 .pipe(replace(/<custom-element-warning>(.+?)<\/custom-element-warning>/g,
                     addCustomElementWarning))
                 .pipe(replace("<page-share></page-share>", addShareWidget))
-                .pipe(replace(/<lazy-youtube(.+?)\/>/g, addLazyYouTube))
                 .pipe(realFavicon.injectFaviconMarkups(
                     JSON.parse(
                         fs.readFileSync(FAVICON_DATA_FILE)
--- server.js
+++ server.js
@@ -44,9 +44,8 @@ server.listen(8080, function (err, address) {
 
 function setHeaders(res, path, stat){
     if (path.endsWith(".html")) {
-        res.setHeader("Content-Security-Policy", "default-src 'self'; img-src 'self' data: https://img.youtube.com; " +
-            "object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'self'; " +
-            "frame-src 'self' https://www.youtube.com;");
+        res.setHeader("Content-Security-Policy", "default-src 'self'; img-src 'self' data:; " +
+            "object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'self';");
         res.setHeader("Referrer-Policy", "same-origin");
         res.setHeader("Feature-Policy", "sync-xhr 'none'; sync-script 'none'; legacy-image-formats 'none'; " +
             "accelerometer 'none'; ambient-light-sensor 'none'; camera 'none'; gyroscope 'none'; " +
